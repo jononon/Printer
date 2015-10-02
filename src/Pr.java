fb2acfcae23009c6b590257196d3005d4c89a25b
Jonathan Damico <jj_damico@yahoo.com>
2015-10-02 11:49:44 -0700

    add printer class

---
+++ src/Pr.java
@@ -0,0 +1,47 @@
+/**
+ * The printer class makes typing System.out.println(); way easier, by offering a new way to print to the command line.
+ * 
+ * To use, simply call .pl for System.out.println(); or .p for System.out.print();
+ * 
+ * @version 0.1
+ * @since 2015
+ * @author Jonathan Damico
+ *
+ */
+public class Pr {
+
+	public static void pl(String output) 	{ System.out.println(output); }
+	
+	public static void pl(int output)		{ System.out.println(output); }
+	
+	public static void pl(long output)		{ System.out.println(output); }
+	
+	public static void pl(float output)		{ System.out.println(output); }
+	
+	public static void pl(double output)	{ System.out.println(output); }
+	
+	public static void pl(char output)		{ System.out.println(output); }
+	
+	public static void pl(boolean output)	{ System.out.println(output); }
+	
+	public static void pl(Object output)	{ System.out.println(output); }
+	
+	public static void pl()					{ System.out.println(); }
+	
+	public static void p(String output) 	{ System.out.print(output); }
+	
+	public static void p(int output)		{ System.out.print(output); }
+	
+	public static void p(long output)		{ System.out.print(output); }
+	
+	public static void p(float output)		{ System.out.print(output); }
+	
+	public static void p(double output)		{ System.out.print(output); }
+	
+	public static void p(char output)		{ System.out.print(output); }
+	
+	public static void p(boolean output)	{ System.out.print(output); }
+	
+	public static void p(Object output)		{ System.out.print(output); }
+	
+}
